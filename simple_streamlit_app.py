ea7494f27d1f9e68f729221c30c3a86615746b34
karaaba <karaaba@karaaba-rtx3060>
2024-06-28 19:59:19 +0200

    a simple streamlit app

---
+++ simple_streamlit_app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from PIL import Image, ImageOps
+
+# Title
+st.title("Hello, Streamlit!")
+
+# Header
+st.header("Welcome to my first Streamlit app")
+
+# Text
+st.write("This is a simple Streamlit app example.")
+
+if 'switch' not in st.session_state:
+    st.session_state.switch = 0
+
+image_placeholder = st.empty()
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+image_org = Image.open(uploaded_file)
+image_placeholder.image(image_org, caption='Brand:',use_column_width=True)
