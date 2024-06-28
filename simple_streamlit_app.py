eb525339f439327a4043d84b5a92e28b66906731
karaaba80 <72360456+karaaba80@users.noreply.github.com>
2024-06-28 20:30:23 +0200

    Update simple_streamlit_app.py
    custom_net is readded... let's see what will happen for streamlit

--- simple_streamlit_app.py
+++ simple_streamlit_app.py
@@ -9,37 +9,37 @@ import torch
 import torch.nn as nn
 import torchvision.models as models
 
-# class CustomNet(nn.Module):
-#     def __init__(self, num_classes, in_features_size=256, adaptive_pool_output=(1, 1), pretrained=True):  # resnet18
-#         super(CustomNet, self).__init__()
+class CustomNet(nn.Module):
+    def __init__(self, num_classes, in_features_size=256, adaptive_pool_output=(1, 1), pretrained=True):  # resnet18
+        super(CustomNet, self).__init__()
 
-#         # model_resnet18 = models.resnet18(pretrained=True)
-#         # torch.save(model_resnet18.state_dict(), 'resnet18_weights.pth')
+        # model_resnet18 = models.resnet18(pretrained=True)
+        # torch.save(model_resnet18.state_dict(), 'resnet18_weights.pth')
 
-#         self.custom_model = nn.Sequential(*list(models.resnet18(pretrained=False).children())[:-3])
+        self.custom_model = nn.Sequential(*list(models.resnet18(pretrained=False).children())[:-3])
 
-#         # if pretrained:
-#         #     state_dict = torch.load('resnet18_weights.pth')
-#         #     self.custom_model.load_state_dict(state_dict)
+        # if pretrained:
+        #     state_dict = torch.load('resnet18_weights.pth')
+        #     self.custom_model.load_state_dict(state_dict)
 
-#         self.adaptiveAvgPool2d = nn.AdaptiveAvgPool2d(output_size=adaptive_pool_output)
+        self.adaptiveAvgPool2d = nn.AdaptiveAvgPool2d(output_size=adaptive_pool_output)
 
-#         new_size = in_features_size * adaptive_pool_output[0] * adaptive_pool_output[1]
-#         self.fc = nn.Linear(new_size, num_classes)
-#         self.adaptive_pool_output = adaptive_pool_output
-#         self.num_classes = num_classes
+        new_size = in_features_size * adaptive_pool_output[0] * adaptive_pool_output[1]
+        self.fc = nn.Linear(new_size, num_classes)
+        self.adaptive_pool_output = adaptive_pool_output
+        self.num_classes = num_classes
 
-#     def forward(self, x):
-#         try:
-#             x = self.custom_model(x)
-#             x = self.adaptiveAvgPool2d(x)
-#             x = x.view(x.size(0), -1)
-#             x = self.fc(x)
-#         except Exception as E:
-#             print("Exception",E)
-#             print(self.custom_model)
-#             exit()
-#         return x
+    def forward(self, x):
+        try:
+            x = self.custom_model(x)
+            x = self.adaptiveAvgPool2d(x)
+            x = x.view(x.size(0), -1)
+            x = self.fc(x)
+        except Exception as E:
+            print("Exception",E)
+            print(self.custom_model)
+            exit()
+        return x
 
 def main():
     # Title
